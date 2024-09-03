36a70811c1b93b0e16d2a97a4166a0df10cce398
meo-young <meoyoung000@naver.com>
2024-09-03 20:01:29 +0900

    NPC 추천 씬 구현

---
+++ Assets/Scripts/meoyoung/Recommend/RecommendNPC.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class RecommendNPC : MonoBehaviour
+{
+    [Tooltip("모든 NPC 넣어주면 돼요")]
+    [SerializeField] List<GameObject> npc;
+
+    [Tooltip("Fade In 연출 이후 몇 초 후에 목소리를 재생할 건지")]
+    [SerializeField] float waitSeconds = 1.0f;
+    // 유나 : 0
+    // 김씨 : 1
+    // 춘자 : 2
+    // 철수 : 3
+    private void Start()
+    {
+        Recommend(0, "안녕하세여");
+    }
+
+    public void Recommend(int num, string text) // 해당하는 npc를 활성화하고, 받은 text를 tts로 재생함
+    {
+        if (num >= 0 && num <= npc.Count - 1)
+        {
+            StartCoroutine(ShowNPCandPlayTTS(num, text));
+        }
+        else
+        {
+            Debug.Log("NPC index가 범위를 초과했습니다.");
+        }
+    }
+
+    IEnumerator ShowNPCandPlayTTS(int num, string text)
+    {
+        FadeController.Instance.JustFade();
+        FadeController.Instance.StartFadeOut();
+        npc[num].SetActive(true);
+        yield return new WaitForSeconds(waitSeconds);
+        PlayTTS(num, text);
+    }
+
+    public void PlayTTS(int num, string sentence)
+    {
+        Debug.Log("대입 문장 : " + sentence);
+        AudioSource audio = GetComponent<AudioSource>();
+
+        // API 발급하고 받은 client_id, client_secret 작성
+        string client_id = "jgw5cfcbb4";
+        string client_secret = "m3XpLu0sLXzlO2DA1IcCevQQtbefRqITJVukm2kd";
+
+        string url = "https://naveropenapi.apigw.ntruss.com/tts-premium/v1/tts";
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+        request.Headers.Add("X-NCP-APIGW-API-KEY-ID", client_id);
+        request.Headers.Add("X-NCP-APIGW-API-KEY", client_secret);
+        request.Method = "POST";
+
+        // 목소리 이름 지정 
+        string avatar_name = npc[num].name;
+        byte[] byteDataParams = Encoding.UTF8.GetBytes($"speaker={avatar_name}&volume=0&speed=0&pitch=0&format=wav&text={sentence}");
+
+        request.ContentType = "application/x-www-form-urlencoded";
+        request.ContentLength = byteDataParams.Length;
+
+        // TTS 요청
+        Stream st = request.GetRequestStream();
+        st.Write(byteDataParams, 0, byteDataParams.Length);
+        st.Close();
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+        // stream 으로 요청 값 받음 
+        Stream input = response.GetResponseStream();
+
+        // stream to byte
+        var memoryStream = new MemoryStream();
+        input.CopyTo(memoryStream);
+        byte[] byteArray = memoryStream.ToArray();
+        float[] f = ConvertByteToFloat(byteArray);
+
+        // byte to audioclip 
+        using (Stream s = new MemoryStream(byteArray))
+        {
+            AudioClip audioClip = AudioClip.Create("tts123", f.Length, 1, 24000, false);
+            audioClip.SetData(f, 0);
+            audio.clip = audioClip;
+            audio.Play();
+        }
+    }
+
+    private float[] ConvertByteToFloat(byte[] array)
+    {
+        float[] floatArr = new float[array.Length / 2];
+        for (int i = 0; i < floatArr.Length; i++)
+        {
+            floatArr[i] = BitConverter.ToInt16(array, i * 2) / 32768.0f;
+        }
+        return floatArr;
+    }
+}
